bf796fadb0531fcb00f984bec5ac442d8f702682
angelika <dehainaut.angelique@orange.fr>
2023-03-28 12:15:57 +0200

    video designer du web

--- js/app.js
+++ js/app.js
@@ -1,36 +1,51 @@
-async function fetchUsers() {
-    //Pour récupérer les données
-    // const r = await fetch('https://jsonplaceholder.typicode.com/users', {
-    //     method: 'GET',
-    //     headers: {
-    //         "Accept" : "application/json",
-    //     },
-    // });
-    //Pour envoyer les données
-    const r = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-            "Accept" : "application/json",
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify({title: 'Mon premier titre'})
-    })
-    if (r.ok === true) {
-        return r.json();
-    }
-    throw new Error('Impossible de contacter le serveur');
-}
-
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(r=> r.json())
-//     .then()
-
-    //Pour récuperer 2 appels
-    //
-    // Promise.race([
-    //     fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5&_delay=2000'),
-    //     fetch('https://jsonplaceholder.typicode.com/users/?_limit=3')
-    // ]).then(r => r.json()).then(console.log)
-
+// async function fetchUsers() {
+//     //Pour récupérer les données
+//     // const r = await fetch('https://jsonplaceholder.typicode.com/users', {
+//     //     method: 'GET',
+//     //     headers: {
+//     //         "Accept" : "application/json",
+//     //     },
+//     // });
+//     //Pour envoyer les données
+//     const r = await fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'POST',
+//         headers: {
+//             "Accept" : "application/json",
+//             "Content-Type" : "application/json",
+//         },
+//         body: JSON.stringify({title: 'Mon premier titre'})
+//     })
+//     if (r.ok === true) {
+//         return r.json();
+//     }
+//     throw new Error('Impossible de contacter le serveur');
 // }
+//
+// // fetch('https://jsonplaceholder.typicode.com/users')
+// //     .then(r=> r.json())
+// //     .then()
+//
+//     //Pour récuperer 2 appels
+//     //
+//     // Promise.race([
+//     //     fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5&_delay=2000'),
+//     //     fetch('https://jsonplaceholder.typicode.com/users/?_limit=3')
+//     // ]).then(r => r.json()).then(console.log)
+//
+// // }
 
+
+//Video du designer
+
+const img = document.getElementById('img');
+
+fetch('https://api.thecatapi.com/v1/images/search')
+    .then(res => {
+        if (res.ok) {
+            res.json().then(data => {
+                img.src = data[0].url
+            })
+        }else {
+            document.getElementById('erreur').innerHTML = "Erreur :("
+        }
+    })
